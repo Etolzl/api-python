84e30a17f61888544bd274e8fce9255e7a3348a9
Etolzl <3tolzlgt.21@gmail.com>
2025-07-23 23:14:17 -0600

    Up Commit

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+# Agrega al inicio del archivo
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Configura CORS si es necesario
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
